d829567135971a673ffd5728bd468da4f8ed0b36
takumabo <kingcongyatuhashi.1043@gmail.com>
2019-06-20 22:07:35 +0800

    全部のバグを修正
    うまくコミットしていなかったので、全ファイルを精製した

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'DiaryController@index')->name('diary.index');
+Route::get('diary/create', 'DiaryController@create')->name('diary.create'); // 投稿画面
+Route::post('diary/create', 'DiaryController@store')->name('diary.create'); // 保存処理
+
+//{} -> ワイルドカード・・・なんでもいい値を入れたいときにこのカードを使う
+Route::delete('diary/{id}/delete', 'DiaryController@destroy')->name('diary.destroy'); // 削除処理
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
